50a4c051fdb947395e3e7736b4115f3f42515c18
maxperei <maxperei@student.42lyon.fr>
2022-05-15 23:42:06 +0200

    Add correct window size creation

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maxperei <maxperei@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: tulipe <tulipe@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 16:11:56 by maxperei          #+#    #+#             */
-/*   Updated: 2022/05/15 18:26:38 by maxperei         ###   ########lyon.fr   */
+/*   Updated: 2022/05/15 23:37:53 by tulipe           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,9 @@ int	main(int argc, char **argv)
 		ft_printf("Error\n");
 		return (0);
 	}
-	//vars.mlx = mlx_init();
-	//vars.mlx_win = mlx_new_window(vars.mlx, xxx, yyy, "so_long");
-	//mlx_loop(vars.mlx);
+	vars.mlx = mlx_init();
+	vars.mlx_win = mlx_new_window(vars.mlx, vars.map.height * 64,
+	vars.map.width * 64, "so_long");
+	mlx_loop(vars.mlx);
 	return (0);
 }
